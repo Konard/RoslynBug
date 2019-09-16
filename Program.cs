cf9a576dc3ec56f9d7cd6fbb62e398b68b850daf
Константин Дьяченко <konard@me.com>
2019-09-16 19:30:45 +0300

    Initial code commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoslynBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
